dd74af7f1162a84ee248ad036233993e15700a12
gregorydebeure <gregorydebeure@gmail.com>
2026-07-11 18:28:19 +0400

    Add files via upload

---
+++ app_test.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_title="Test", page_icon="✈")
+st.title("✈ Aviation Cost Estimator")
+st.success("App is running!")
+st.write("If you see this, the infrastructure works.")
